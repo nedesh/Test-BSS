321561fb49dc6d411e2d66e9a686a9b24fc60f40
Nedesh <andrey.deshankov@gmail.com>
2023-10-26 15:19:03 +0300

    fix

--- src/main/java/rouletteGame/RouletteSimulation.java
+++ src/main/java/rouletteGame/RouletteSimulation.java
@@ -1,18 +1,21 @@
 package rouletteGame;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+@Component
 @RequiredArgsConstructor
 public class RouletteSimulation {
-    @NonNull
-    private int totalExperiments;
+    private final RouletteConfig rouletteConfig;
     private double firstPlayerSurvival;
     private double secondPlayerSurvivalRotate;
     private double secondPlayerSurvivalShoot;
 
-    public void runSimulation(boolean useBarrelRotation) {
+    private void runSimulation() {
+        int totalExperiments = rouletteConfig.getTotalExperiments();
+        boolean useBarrelRotation = rouletteConfig.isUseBarrelRotation();
         Random random = new Random();
         double firsShotProbability = 2.0 / 6.0;
         double secondShotProbabilityWithRotate = 2.0 / 6.0; //Вероятность смертельного выстрела становится исходной при прокрутке
@@ -34,7 +37,10 @@ public class RouletteSimulation {
         }
     }
 
-    public void printResults(boolean useBarrelRotation) {
+    private void printResults() {
+        int totalExperiments = rouletteConfig.getTotalExperiments();
+        boolean useBarrelRotation = rouletteConfig.isUseBarrelRotation();
+
         System.out.println("Total experiments: " + totalExperiments);
         System.out.println("First Player Survival Percentage: " + (firstPlayerSurvival / totalExperiments) * 100 + "%");
         if(useBarrelRotation) {
@@ -42,5 +48,10 @@ public class RouletteSimulation {
         } else {
             System.out.println("Second Player Survival Percentage (Shoot): " + (secondPlayerSurvivalShoot / totalExperiments) * 100 + "%");
         }
+    }
+
+    public static void startGame(RouletteSimulation simulation) {
+        simulation.runSimulation();
+        simulation.printResults();
     }
 }
